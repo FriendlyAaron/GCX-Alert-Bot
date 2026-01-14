afd272729b2b352248f33ab58399d582a7474dab
FriendlyAaron <84158176+FriendlyAaron@users.noreply.github.com>
2026-01-14 00:30:11 -0500

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,15 +1,20 @@
 import smtplib
 import os
 from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv("info.env")
+EMAIL = os.getenv("EMAIL")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 def email_alert(subject, body, to):
   msg = EmailMessage()
   msg.set_content(body)
   msg['subject'] = subject
   msg['to'] = to
-  email =os.environ ['email']
-  emailPassword =os.environ ['emailPassword']
   msg['from'] = email
+  
+  # Connect to SMTP server
   server = smtplib.SMTP("smtp.gmail.com", 587)
   server.starttls()
   server.login(email, emailPassword)
